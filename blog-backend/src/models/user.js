ff6576d5af6c685232e0ddc4a7ee2f9ffbbaebe3
dongchan-K <ehdcks1028@gmail.com>
2021-06-24 14:44:43 +0900

    feat: User 스키마, 모델 생성

---
+++ blog-backend/src/models/user.js
@@ -0,0 +1,28 @@
+import mongoose, { Schema } from 'mongoose';
+import bcrypt from 'bcrypt';
+
+const UserSchema = new Schema({
+  username: String,
+  hashedPassword: String,
+});
+
+// 인스턴스 메서드 작성 시 화살표 함수가 아닌 function 키워드를 사용해야 한다 -> 함수 내부에서 this에 접근해야 하기 때문
+// this는 문서 인스턴스를 가리킨다
+UserSchema.methods.setPassword = async function (password) {
+  const hash = await bcrypt.hash(password, 10);
+  this.hashedPassword = hash;
+};
+
+UserSchema.methods.checkPassword = async function (password) {
+  const result = await bcrypt.compare(password, this.hashedPassword);
+  return result; // true or false
+};
+
+// 스태틱 메서드의 this는 모델을 가리킨다 -> User
+UserSchema.statics.findByUsername = function (username) {
+  return this.findOne({ username });
+};
+
+const User = mongoose.model('User', UserSchema);
+
+export default User;
