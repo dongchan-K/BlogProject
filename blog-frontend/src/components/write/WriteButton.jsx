3c6054bd91c7a29579d2ea02784d02e1291228b2
dongchan-K <ehdcks1028@gmail.com>
2021-07-07 00:46:47 +0900

    feat: 게시물 등록 및 취소 버튼 UI 구현

---
+++ blog-frontend/src/components/write/WriteButton.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import styled from 'styled-components';
+import Button from '../common/Button';
+
+const WriteButtonWrapper = styled.div`
+  margin: 1rem 1rem 3rem 1rem;
+  button + button {
+    margin-left: 0.5rem;
+  }
+`;
+
+const StyledButton = styled(Button)`
+  height: 2.125rem;
+  /* & + & {
+    margin-left: 0.5rem;
+  } */
+`;
+
+const WriteButton = ({ onCancel, onPublish }) => {
+  return (
+    <WriteButtonWrapper>
+      <StyledButton cyan onClick={onPublish}>
+        포스트 등록
+      </StyledButton>
+      <StyledButton onClick={onCancel}>취소</StyledButton>
+    </WriteButtonWrapper>
+  );
+};
+
+export default WriteButton;
--- blog-frontend/src/pages/WritePage.jsx
+++ blog-frontend/src/pages/WritePage.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import Responsive from '../components/common/Responsive';
 import Editor from '../components/write/Editor';
 import TagBox from '../components/write/TagBox';
+import WriteButton from '../components/write/WriteButton';
 
 const WritePage = () => {
   return (
     <Responsive>
       <Editor />
       <TagBox />
+      <WriteButton />
     </Responsive>
   );
 };
