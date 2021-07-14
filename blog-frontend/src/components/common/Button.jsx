9c24eed98c130b0192b3be30ffae82c7c6a638b2
dongchan-K <ehdcks1028@gmail.com>
2021-07-14 14:09:53 +0900

    feat: 프론트엔드 프로젝트 빌드

--- blog-frontend/src/components/common/Button.jsx
+++ blog-frontend/src/components/common/Button.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled, { css } from 'styled-components';
-import palette from '../../lib/styles/palette';
 import { Link } from 'react-router-dom';
+import palette from '../../lib/styles/palette';
 
 const buttonStyle = css`
   align-items: center;
